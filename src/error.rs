9b8b24ba06bd666f172b7e360a65728a9b305b9c
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-24 22:43:12 +0100

    Add macro documentation

--- src/error.rs
+++ src/error.rs
@@ -15,8 +15,11 @@ impl Debug for MagickError {
 
 impl std::error::Error for MagickError {}
 
+/// Similar to `format!`, but returns a `MagickError` and also records the source code location where it was called.
+/// We use it to imitate the structure of imagemagick's error messages.
 #[macro_export]
 macro_rules! wm_err {
+    // TODO: consider also recording function path: https://stackoverflow.com/a/40234666/585725
     ($msg:expr) => {
         MagickError(format!(
             "wondermagick: {} @ {}:{}:{}",
@@ -28,6 +31,8 @@ macro_rules! wm_err {
     };
 }
 
+/// Similar to the `try!` macro and the `?` operator, but also
+/// records the source code location where it was called.
 #[macro_export]
 macro_rules! wm_try {
     ($expr:expr $(,)?) => {
