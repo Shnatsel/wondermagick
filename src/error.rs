7da906ecdbb18118267fcc9ff6c9ef03be6aaadf
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-24 21:45:28 +0100

    Add an error type

---
+++ src/error.rs
@@ -0,0 +1,39 @@
+use std::fmt::{Debug, Display};
+pub struct MagickError(String);
+
+impl Display for MagickError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+impl Debug for MagickError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("MagickError").field(&self.0).finish()
+    }
+}
+
+impl std::error::Error for MagickError {}
+
+macro_rules! wm_err {
+    ($msg:expr) => {
+        MagickError(format!(
+            "wondermagick: {} @ {}:{}:{}",
+            $msg,
+            file!(),
+            line!(),
+            column!()
+        ))
+    };
+}
+
+macro_rules! wm_try {
+    ($expr:expr $(,)?) => {
+        match $expr {
+            std::result::Result::Ok(val) => val,
+            std::result::Result::Err(err) => {
+                return std::result::Result::Err(wm_err!(err));
+            }
+        }
+    };
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+//! `wondermagick` is not a library.
+//! This interface is unstable and subject to change at any time.
+//! Please use this documentation only if you are developing `wondermagick`.
 #![forbid(unsafe_code)]
 
 mod arg_parsers;
 pub mod args;
+mod error;
 mod operations;
 mod plan;
