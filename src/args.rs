465a6ae333f694642bb7adc32d02a5c832143187
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-24 22:44:01 +0100

    cargo fmt

--- src/args.rs
+++ src/args.rs
@@ -12,7 +12,11 @@ use std::{
 use image::DynamicImage;
 
 use crate::{
-    arg_parsers::ResizeGeometry, error::MagickError, operations, plan::{ExecutionPlan, FilePlan}, wm_err, wm_try
+    arg_parsers::ResizeGeometry,
+    error::MagickError,
+    operations,
+    plan::{ExecutionPlan, FilePlan},
+    wm_err, wm_try,
 };
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -74,8 +78,12 @@ impl Arg {
         };
 
         match self {
-            Arg::Resize => Ok(Operation::Resize(wm_try!(ResizeGeometry::try_from(value.unwrap())))),
-            Arg::Thumbnail => Ok(Operation::Thumbnail(wm_try!(ResizeGeometry::try_from(value.unwrap())))),
+            Arg::Resize => Ok(Operation::Resize(wm_try!(ResizeGeometry::try_from(
+                value.unwrap()
+            )))),
+            Arg::Thumbnail => Ok(Operation::Thumbnail(wm_try!(ResizeGeometry::try_from(
+                value.unwrap()
+            )))),
         }
     }
 }
@@ -109,7 +117,10 @@ pub fn parse_args(mut args: Vec<OsString>) -> Result<ExecutionPlan, MagickError>
     let output_filename = args.pop().unwrap();
     // imagemagick rejects output filenames that look like arguments
     if starts_with_dash(&output_filename) {
-        return Err(wm_err!(format!("missing an image filename `{}'", output_filename.to_string_lossy())));
+        return Err(wm_err!(format!(
+            "missing an image filename `{}'",
+            output_filename.to_string_lossy()
+        )));
     }
 
     let mut plan = ExecutionPlan::default();
@@ -125,10 +136,16 @@ pub fn parse_args(mut args: Vec<OsString>) -> Result<ExecutionPlan, MagickError>
             // A file named "-foobar.jpg" will be parsed as an option.
             // Sadly imagemagick does not support the -- convention to separate options and filenames,
             // and there is nothing we can do about it without introducing incompatibility in argument parsing.
-            let string_arg = arg.to_str().ok_or(wm_err!(format!("unrecognized option `{}'", arg.to_string_lossy())))?;
+            let string_arg = arg.to_str().ok_or(wm_err!(format!(
+                "unrecognized option `{}'",
+                arg.to_string_lossy()
+            )))?;
             let arg_name = Arg::from_str(string_arg)?;
             let operation = if arg_name.needs_value() {
-                let value = iter.next().ok_or(wm_err!(format!("argument requires a value: {}", &string_arg)))?;
+                let value = iter.next().ok_or(wm_err!(format!(
+                    "argument requires a value: {}",
+                    &string_arg
+                )))?;
                 arg_name.to_operation(Some(value.as_os_str()))?
             } else {
                 arg_name.to_operation(None)?
