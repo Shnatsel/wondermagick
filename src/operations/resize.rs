1e499ef0508c1324c4b8880d0c8085712a959b72
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-25 13:41:32 +0100

    Support only shrinking, only enlarging, and preserving aspect ratio when resizing

--- src/operations/resize.rs
+++ src/operations/resize.rs
@@ -1,7 +1,10 @@
 use fast_image_resize::{ResizeAlg, ResizeOptions, Resizer};
 use image::DynamicImage;
 
-use crate::arg_parsers::{ResizeConstraint, ResizeGeometry};
+use crate::{
+    arg_parsers::{ResizeConstraint, ResizeGeometry},
+    utils::fraction::Fraction,
+};
 
 pub fn resize(image: &mut DynamicImage, geometry: &ResizeGeometry) {
     let (dst_width, dst_height) = compute_dimensions(image, geometry);
@@ -34,19 +37,22 @@ fn resize_impl(image: &mut DynamicImage, dst_width: u32, dst_height: u32, algori
 }
 
 fn compute_dimensions(image: &DynamicImage, geometry: &ResizeGeometry) -> (u32, u32) {
-    let width = compute_dimension(image.width(), geometry.width, geometry);
-    let height = compute_dimension(image.height(), geometry.height, geometry);
+    let mut width = compute_dimension(image.width(), geometry.width, geometry);
+    let mut height = compute_dimension(image.height(), geometry.height, geometry);
+    if !geometry.ignore_aspect_ratio {
+        (width, height) = preserve_aspect_ratio(image, width, height);
+    }
     (width, height)
 }
 
-fn compute_dimension(image_size: u32, geom_size: Option<u32>, geometry: &ResizeGeometry) -> u32 {
+fn compute_dimension(image_size: u32, target_size: Option<u32>, geometry: &ResizeGeometry) -> u32 {
     // If no size is specified for this dimension, keep the image's original size
-    let geom_size = geom_size.unwrap_or(image_size);
+    let target_size = target_size.unwrap_or(image_size);
 
     let size = match geometry.constraint {
-        ResizeConstraint::Any => geom_size,
-        ResizeConstraint::OnlyEnlarge => image_size.max(geom_size),
-        ResizeConstraint::OnlyShrink => image_size.min(geom_size),
+        ResizeConstraint::Any => target_size,
+        ResizeConstraint::OnlyEnlarge => image_size.max(target_size),
+        ResizeConstraint::OnlyShrink => image_size.min(target_size),
     };
 
     // imagemagick emits a 1x1 image if you ask for a 0x0 one
@@ -54,5 +60,54 @@ fn compute_dimension(image_size: u32, geom_size: Option<u32>, geometry: &ResizeG
         1
     } else {
         size
+    }
+}
+
+/// Returns `(width, height)`
+fn preserve_aspect_ratio(
+    image: &DynamicImage,
+    target_width: u32,
+    target_height: u32,
+) -> (u32, u32) {
+    let image_ratio = Fraction::new(image.width(), image.height());
+    let target_ratio = Fraction::new(target_width, target_height);
+    use std::cmp::Ordering;
+    match image_ratio.cmp(&target_ratio) {
+        Ordering::Less => {
+            // the image is narrower than the target dimensions, reduce width
+            let width = (image_ratio.to_float() * target_height as f64).round() as u32;
+            (width, target_height)
+        }
+        Ordering::Greater => {
+            // the image is wider than the target dimensions, reduce height
+            let height = (image_ratio.reciprocal().to_float() * target_width as f64).round() as u32;
+            (target_width, height)
+        }
+        Ordering::Equal => (target_width, target_height),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use image::DynamicImage;
+
+    use super::preserve_aspect_ratio;
+
+    #[test]
+    fn preserve_aspect_ratio_wide() {
+        let image = DynamicImage::new_rgb8(800, 600);
+        assert_eq!(preserve_aspect_ratio(&image, 100, 100), (100, 75));
+    }
+
+    #[test]
+    fn preserve_aspect_ratio_narrow() {
+        let image = DynamicImage::new_rgb8(600, 800);
+        assert_eq!(preserve_aspect_ratio(&image, 100, 100), (75, 100));
+    }
+
+    #[test]
+    fn preserve_aspect_ratio_same() {
+        let image = DynamicImage::new_rgb8(800, 800);
+        assert_eq!(preserve_aspect_ratio(&image, 100, 100), (100, 100));
     }
 }
