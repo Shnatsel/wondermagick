e5feeb0060c2a4961c379df92bad7914f66293a2
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-27 15:53:56 +0100

    Wire up rotation by Exif to the decoding function

--- src/decode.rs
+++ src/decode.rs
@@ -1,16 +1,23 @@
 use std::ffi::OsStr;
 
-use image::{DynamicImage, ImageDecoder, ImageFormat, ImageReader, ImageResult};
+use image::{DynamicImage, ImageDecoder, ImageFormat, ImageReader};
+
+use crate::{error::MagickError, exif::rotate_by_exif, wm_try};
 
 /// If the format has not been explicitly specified, guesses the format based on file contents.
-pub fn decode(file: &OsStr, format: Option<ImageFormat>) -> ImageResult<DynamicImage> {
-    let mut reader = ImageReader::open(file)?;
+pub fn decode(file: &OsStr, format: Option<ImageFormat>) -> Result<DynamicImage, MagickError> {
+    let mut reader = wm_try!(ImageReader::open(file));
     match format {
         Some(format) => reader.set_format(format),
-        None => reader = reader.with_guessed_format()?,
+        None => reader = wm_try!(reader.with_guessed_format()),
     }
-    let mut decoder = reader.into_decoder()?;
+    let mut decoder = wm_try!(reader.into_decoder());
     let exif = decoder.exif_metadata();
-    // TODO: apply EXIF rotation
-    DynamicImage::from_decoder(decoder)
+    let mut image = wm_try!(DynamicImage::from_decoder(decoder));
+    if let Ok(Some(exif)) = exif {
+        // we ignore errors here because a malformed exif orientation
+        // should not cause the entire decoding to fail
+        let _ = rotate_by_exif(&mut image, exif);
+    }
+    Ok(image)
 }
