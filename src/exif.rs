59eaab33d9b6c8c3a01d51e67770e3ff649f59ec
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-27 15:42:00 +0100

    Add a function to rotate by exif from raw exif

--- src/exif.rs
+++ src/exif.rs
@@ -1,9 +1,23 @@
+use exif::{In, Tag};
 use image::DynamicImage;
 
-pub fn apply_exif_orientation(
-    image: &mut DynamicImage,
-    orientation: u8,
-) -> Result<(), &'static str> {
+use crate::{error::MagickError, wm_err, wm_try};
+
+pub fn rotate_by_exif(image: &mut DynamicImage, raw_exif: Vec<u8>) -> Result<(), MagickError> {
+    let reader = exif::Reader::new();
+    let exif = wm_try!(reader.read_raw(raw_exif));
+    // based on the Exif crate example: https://docs.rs/kamadak-exif/latest/exif/index.html#examples
+    if let Some(orientation) = exif.get_field(Tag::Orientation, In::PRIMARY) {
+        match orientation.value.get_uint(0) {
+            Some(v @ 1..=8) => apply_exif_orientation(image, v as u8).map_err(|e| wm_err!("{}", e)),
+            _ => Err(wm_err!("invalid Exif orientation value")),
+        }
+    } else {
+        Ok(()) // no orientation value in Exif, nothing to do
+    }
+}
+
+fn apply_exif_orientation(image: &mut DynamicImage, orientation: u8) -> Result<(), &'static str> {
     // An explanation of Exif orientation:
     // https://web.archive.org/web/20200412005226/https://www.impulseadventure.com/photo/exif-orientation.html
     match orientation {
@@ -25,6 +39,6 @@ pub fn apply_exif_orientation(
             Ok(())
         }
         8 => Ok(*image = image.rotate270()),
-        0 | 9.. => return Err("Invalid Exif orientation value"),
+        0 | 9.. => return Err("invalid Exif orientation value"),
     }
 }
