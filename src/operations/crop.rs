3a562b358548e151ab680a8ed86263f5ee57867b
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-28 21:40:36 +0100

    ohhh nooooo, the crop API is a lot worse than I thought it was

--- src/operations/crop.rs
+++ src/operations/crop.rs
@@ -1,10 +1,14 @@
-use crate::{arg_parsers::LoadCropGeometry, wm_try};
+use crate::arg_parsers::LoadCropGeometry;
 
 pub fn crop_on_load(
     image: &mut image::DynamicImage,
     geom: &LoadCropGeometry,
 ) -> Result<(), crate::error::MagickError> {
-    let cropped = wm_try!(image.crop_imm(geom.xoffset, geom.yoffset, geom.width, geom.height));
+    // Sadly this doesn't check bounds right now, so we can get panics later on because of wrong crop parameters:
+    // https://github.com/image-rs/image/issues/2296
+    // TODO: change this in `image` because I don't want to emulate this on the client side
+    // and pretend the problem doesn't exist for anyone else
+    let cropped = image.crop_imm(geom.xoffset, geom.yoffset, geom.width, geom.height);
     *image = cropped;
     Ok(())
 }
