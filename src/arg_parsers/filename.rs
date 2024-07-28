fa3f64596812de20970158cd8162845516a9c25c
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-28 21:12:24 +0100

    Implement reading of resize directives on loading (not wired up yet)

--- src/arg_parsers/filename.rs
+++ src/arg_parsers/filename.rs
@@ -11,7 +11,7 @@ use super::{Geometry, ResizeGeometry};
 pub struct InputFileArg {
     path: PathBuf,
     //format: Option<String>, // TODO: turn into an enum and enable
-    load_op: LoadOperation,
+    read_mod: ReadModifier,
 }
 
 /// The action to be taken upon loading the image.
@@ -19,12 +19,37 @@ pub struct InputFileArg {
 ///
 /// See <https://imagemagick.org/Usage/files/#read_mods> for details.
 /// I've also verified it behaves according to the documentation.
-pub enum LoadOperation {
+pub enum ReadModifier {
     Resize(ResizeGeometry),
     Crop(LoadCropGeometry),
     // TODO: frame selection.
 }
 
+impl TryFrom<&OsStr> for ReadModifier {
+    type Error = MagickError;
+
+    fn try_from(s: &OsStr) -> Result<Self, Self::Error> {
+        if !s.is_ascii() {
+            return Err(wm_err!("invalid read modifier: {}", s.to_string_lossy()));
+        }
+
+        let ascii = s.as_encoded_bytes();
+        let x_count = ascii.iter().copied().filter(|c| *c == b'x').take(2).count();
+        let plus_count = ascii.iter().copied().filter(|c| *c == b'+').take(3).count();
+
+        if x_count == 1 && plus_count == 0 {
+            Ok(Self::Resize(ResizeGeometry::try_from(s)?))
+        } else if x_count == 1 && plus_count == 2 {
+            Ok(Self::Crop(LoadCropGeometry::try_from(s)?))
+        } else {
+            Err(wm_err!("frame selection is not yet supported")) // TODO: support [0] at least
+        }
+    }
+}
+
+/// On loading only a subset of crop geometry specification is supported:
+/// it *must* be in the form AxB+C+D, see
+/// https://imagemagick.org/Usage/files/#read_mods
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub struct LoadCropGeometry {
     pub width: u32,
@@ -45,26 +70,16 @@ impl TryFrom<&OsStr> for LoadCropGeometry {
     type Error = MagickError;
 
     fn try_from(s: &OsStr) -> Result<Self, Self::Error> {
-        if !s.is_ascii() {
-            return Err(wm_err!("invalid crop geometry: {}", s.to_string_lossy()));
-        }
-
-        // On loading only a subset of crop geometry specification is supported:
-        // it *must* be in the form AxB+C+D, see
-        // https://imagemagick.org/Usage/files/#read_mods
-        let ascii = s.as_encoded_bytes();
-        let x_count = ascii.iter().copied().filter(|c| *c == b'x').take(2).count();
-        let plus_count = ascii.iter().copied().filter(|c| *c == b'+').take(3).count();
-        if x_count != 1 || plus_count != 2 {
-            return Err(wm_err!("invalid crop geometry: {}", s.to_string_lossy()));
-        }
-
         let geom = Geometry::try_from(s)?;
 
         let convert_field = |field: Option<f64>| -> Result<u32, MagickError> {
-            field
-                .map(|f| f as u32)
-                .ok_or_else(|| wm_err!("invalid crop geometry: {}", s.to_string_lossy()))
+            let f =
+                field.ok_or_else(|| wm_err!("invalid crop geometry: {}", s.to_string_lossy()))?;
+            if f.is_sign_negative() {
+                Err(wm_err!("invalid crop geometry: {}", s.to_string_lossy()))
+            } else {
+                Ok(f as u32)
+            }
         };
 
         Ok(Self {
