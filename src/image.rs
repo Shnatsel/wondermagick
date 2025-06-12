e3f2f200967b1660cb912c8d9c504ba0328973be
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-06-12 18:25:53 +0100

    More comments

--- src/image.rs
+++ src/image.rs
@@ -14,5 +14,6 @@ impl Image {
     pub fn save(&self, output_file: &OsStr) -> Result<(), MagickError> {
         Ok(wm_try!(self.pixels.save(output_file)))
         // TODO: Exif, ICC once they are implemented in `image`
+        // TODO: handle -strip flag here. It's not an operation because `-strip -auto-orient` works.
     }
 }
