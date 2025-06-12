203b03955b6e5efadb0f60b45cd5f9e9c0c711f4
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-06-12 17:49:48 +0100

    Add forgotten file

---
+++ src/image.rs
@@ -0,0 +1,18 @@
+use std::ffi::OsStr;
+
+use crate::{error::MagickError, wm_try};
+use image::DynamicImage;
+
+#[derive(Debug, Clone)]
+pub struct Image {
+    pub exif: Option<Vec<u8>>,
+    pub icc: Option<Vec<u8>>,
+    pub pixels: DynamicImage,
+}
+
+impl Image {
+    pub fn save(&self, output_file: &OsStr) -> Result<(), MagickError> {
+        Ok(wm_try!(self.pixels.save(output_file)))
+        // TODO: Exif, ICC
+    }
+}
