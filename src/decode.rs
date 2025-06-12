bb7cc91e9534a11c079404b192681b32226a00b7
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-06-12 17:43:02 +0100

    Refactor decoding to return crate::Image

--- src/decode.rs
+++ src/decode.rs
@@ -2,19 +2,18 @@ use std::ffi::OsStr;
 
 use image::{DynamicImage, ImageDecoder, ImageFormat, ImageReader};
 
-use crate::{error::MagickError, wm_try};
+use crate::{error::MagickError, image::Image, wm_try};
 
 /// If the format has not been explicitly specified, guesses the format based on file contents.
-pub fn decode(file: &OsStr, format: Option<ImageFormat>) -> Result<DynamicImage, MagickError> {
+pub fn decode(file: &OsStr, format: Option<ImageFormat>) -> Result<Image, MagickError> {
     let mut reader = wm_try!(ImageReader::open(file));
     match format {
         Some(format) => reader.set_format(format),
         None => reader = wm_try!(reader.with_guessed_format()),
     }
     let mut decoder = wm_try!(reader.into_decoder());
-    let orientation = wm_try!(decoder.orientation());
-    let mut image = wm_try!(DynamicImage::from_decoder(decoder));
-    // TODO: apply orientation only if -auto-orient is passed
-    image.apply_orientation(orientation);
-    Ok(image)
+    let exif = decoder.exif_metadata().unwrap_or(None);
+    let icc = decoder.icc_profile().unwrap_or(None);
+    let pixels = wm_try!(DynamicImage::from_decoder(decoder));
+    Ok(Image { exif, icc, pixels })
 }
