22501b5b863b7a4ee8f50f5910cdcd81d945aa3a
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-06-13 03:53:11 +0100

    Add a comment on thumbnail images embedded in Exif metadata

--- src/decode.rs
+++ src/decode.rs
@@ -17,3 +17,27 @@ pub fn decode(file: &OsStr, format: Option<ImageFormat>) -> Result<Image, Magick
     let pixels = wm_try!(DynamicImage::from_decoder(decoder));
     Ok(Image { exif, icc, pixels })
 }
+
+// You know what would be a cool optimization for decoding process?
+// Using the image thumbnail from the Exif metadata, when it's present.
+// Many JPEG images have it; other formats can contain it as well.
+// That way you don't even have to decode the full-size image!
+// You can dump it from a JPEG that has one like this:
+// $ exiftool -b -ThumbnailImage image.jpg > ~/thumb.jpg
+// It saves so much time! Every competent thumbnailer uses it!
+//
+// But imagemagick is not a competent thumbnailer.
+// Imagemagick not only ignores it, it does something far worse:
+// It keeps the old thumbnail even after modifying the image.
+// Making the embedded thumbnail WRONG.
+//
+// Not only does it not make use of the thumbnail that someone
+// has already spent network bandwidth and storage on,
+// but it also breaks any other program making use of it,
+// by keeping the old thumbnail around
+// even when it doesn't match the image anymore.
+//
+// This is completely wrong behavior no matter how you look at it.
+// But that's what imagemagick does and what we must do as well.
+// Until we decide enough is enough and some bugs aren't worth
+// being compatible with, but that day is not today.
