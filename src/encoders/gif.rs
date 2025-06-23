14c00dc8c9a17894b48cce915bfe8c836ab5ce16
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-06-23 01:56:29 +0100

    Convert the incoming image into Rgb(a)8 when encoding WebP. We cannot rely on `image` to do it for us (which it currently doesn't but shhhh) because this encoding bypasses `image`

--- src/encoders/gif.rs
+++ src/encoders/gif.rs
@@ -1,9 +1,11 @@
 use std::io::Write;
 
 use image::codecs::gif::GifEncoder;
-use image::ExtendedColorType;
+use image::{DynamicImage, ExtendedColorType};
 
 use crate::{error::MagickError, image::Image, plan::Modifiers, wm_try};
+// needed because of https://github.com/image-rs/image/issues/2498
+use crate::encoders::webp::to_8bit_color;
 
 pub fn encode<W: Write>(
     image: &Image,
@@ -18,23 +20,14 @@ pub fn encode<W: Write>(
     let pixels = &image.pixels;
     let width = pixels.width();
     let height = pixels.height();
-    use image::DynamicImage::*;
-    let converted_image = match pixels {
-        ImageRgb8(_) => None,
-        ImageRgba8(_) => None,
-        _ => if pixels.has_alpha() {
-            Some(ImageRgba8(pixels.to_rgba8()))
-        } else {
-            Some(ImageRgb8(pixels.to_rgb8()))
-        },
-    };
 
+    let converted_image = to_8bit_color(pixels);
     let image = converted_image.as_ref().unwrap_or(&image.pixels);
     Ok(match image {
-        ImageRgb8(data) => {
+        DynamicImage::ImageRgb8(data) => {
             wm_try!(encoder.encode(data.as_raw(), width, height, ExtendedColorType::Rgb8))
         }
-        ImageRgba8(data) => {
+        DynamicImage::ImageRgba8(data) => {
             wm_try!(encoder.encode(data.as_raw(), width, height, ExtendedColorType::Rgba8))
         }
         _ => unreachable!(), // we've just converted it to RGB(A)
--- src/encoders/webp.rs
+++ src/encoders/webp.rs
@@ -1,6 +1,7 @@
 use std::io::Write;
 
 use crate::{error::MagickError, image::Image, plan::Modifiers, wm_err, wm_try};
+use image::DynamicImage;
 use webp::{Encoder, WebPMemory};
 
 pub fn encode<W: Write>(
@@ -8,17 +9,38 @@ pub fn encode<W: Write>(
     writer: &mut W,
     modifiers: &Modifiers,
 ) -> Result<(), MagickError> {
-    let encoder: Encoder = Encoder::from_image(&image.pixels).unwrap();
+    // Convert the image to Rgb(a)8, because those are the only formats the encoder supports
+    let converted_pixels = to_8bit_color(&image.pixels);
+    let pixels = converted_pixels.as_ref().unwrap_or(&image.pixels);
+
+    let encoder: Encoder = Encoder::from_image(pixels).unwrap();
     // imagemagick signals that the image should be lossless with quality=100
     let lossless = modifiers.quality == Some(100);
     // default quality is not documented, was determined experimentally
     let quality = modifiers.quality.unwrap_or(75) as f32;
 
-    // Encode the image at a specified quality
+    // Encode the image with the specified quality
     let webp: WebPMemory = encoder
         .encode_simple(lossless, quality)
         .map_err(|e| wm_err!("WebP encoding failed: {e:?}"))?;
     // TODO: `webp` crate doesn't support setting the ICC profile:
     // https://github.com/jaredforth/webp/issues/41
     Ok(wm_try!(writer.write_all(&webp)))
+}
+
+/// Converts the input image to Rgb8 or Rgba8, depending on the presence of an alpha channel.
+/// Returns `None` if the image doesn't need conversion.
+pub(crate) fn to_8bit_color(pixels: &DynamicImage) -> Option<DynamicImage> {
+    use image::DynamicImage::*;
+    match pixels {
+        ImageRgb8(_) => None,
+        ImageRgba8(_) => None,
+        _ => {
+            if pixels.color().has_alpha() {
+                Some(ImageRgba8(pixels.to_rgba8()))
+            } else {
+                Some(ImageRgb8(pixels.to_rgb8()))
+            }
+        }
+    }
 }
