639e14e1e1c504f37443c9cad2a36b6a6b9ac385
Jacob Adam <software@jacobadam.net>
2025-08-05 17:58:15 +0100

    Add a fuzz test.

---
+++ fuzz/fuzz_targets/rotate.rs
@@ -0,0 +1,84 @@
+#![no_main]
+
+use std::path::Path;
+
+use arbitrary::Unstructured;
+use bounded_integer::BoundedU16;
+use libfuzzer_sys::fuzz_target;
+
+const MAX_WIDTH: u16 = 1024;
+const MAX_HEIGHT: u16 = 1024;
+
+#[derive(Debug)]
+struct StructuredImage {
+    width: BoundedU16<1, MAX_WIDTH>,
+    height: BoundedU16<1, MAX_HEIGHT>,
+    rgb_data: Vec<u8>,
+}
+
+impl StructuredImage {
+    fn save_as_png(&self, path: impl AsRef<Path>) -> Result<(), std::io::Error> {
+        use image::{codecs::png::PngEncoder, ImageBuffer, ImageEncoder, RgbImage};
+        use std::fs::File;
+
+        let img: RgbImage =
+            ImageBuffer::from_fn(self.width.get() as u32, self.height.get() as u32, |x, y| {
+                let idx = (y * self.width.get() as u32 + x) as usize * 3;
+                image::Rgb([
+                    self.rgb_data[idx],
+                    self.rgb_data[idx + 1],
+                    self.rgb_data[idx + 2],
+                ])
+            });
+
+        let file = File::create(path)?;
+        let encoder = PngEncoder::new_with_quality(
+            file,
+            image::codecs::png::CompressionType::Fast,
+            image::codecs::png::FilterType::NoFilter,
+        );
+        encoder
+            .write_image(
+                &img,
+                self.width.get() as u32,
+                self.height.get() as u32,
+                image::ExtendedColorType::Rgb8,
+            )
+            .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))
+    }
+}
+
+impl<'a> arbitrary::Arbitrary<'a> for StructuredImage {
+    fn arbitrary(unstructured: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+        let width: BoundedU16<1, MAX_WIDTH> = unstructured.arbitrary()?;
+        let height: BoundedU16<1, MAX_HEIGHT> = unstructured.arbitrary()?;
+        let rgb_data_len = width.get() as usize * height.get() as usize * 3;
+        let rgb_data = unstructured.bytes(rgb_data_len)?;
+
+        Ok(Self {
+            width,
+            height,
+            rgb_data: rgb_data.to_vec(),
+        })
+    }
+}
+
+fuzz_target!(|input: (StructuredImage, BoundedU16<0, 359>)| {
+    let (image, angle) = input;
+
+    let temp_directory = tempfile::tempdir().expect("failed to create temporary directory");
+    let input_path = temp_directory.path().join("input_image.png");
+    image
+        .save_as_png(&input_path)
+        .expect("failed to save image as PNG");
+
+    let (_wondermagick_output_image_path, _imagemagick_output_image_path) =
+        wondermagick_fuzz::run_commands_and_compare(
+            &temp_directory,
+            &[
+                input_path.to_str().expect("must be valid"),
+                "-rotate",
+                &angle.to_string(),
+            ],
+        );
+});
