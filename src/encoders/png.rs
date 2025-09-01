0739f358c45c858b7d507c585ad7dd73bfbcf62c
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-09-01 02:39:05 +0100

    correct PNG compression level mapping

--- src/encoders/png.rs
+++ src/encoders/png.rs
@@ -1,6 +1,6 @@
 use std::io::Write;
 
-use image::codecs::png::{CompressionType, FilterType, PngEncoder};
+use image::codecs::png::{DeflateCompressionType, FilterType, PngEncoder};
 
 use crate::encoders::common::write_icc_and_exif;
 use crate::plan::Modifiers;
@@ -13,7 +13,8 @@ pub fn encode<W: Write>(
     modifiers: &Modifiers,
 ) -> Result<(), MagickError> {
     let (compression, filter) = quality_to_compression_parameters(modifiers.quality)?;
-    let mut encoder = PngEncoder::new_with_quality(writer, compression, filter);
+    let mut encoder = PngEncoder::new_with_quality(writer, Default::default(), filter);
+    encoder.set_deflate_compression(compression);
     write_icc_and_exif(&mut encoder, image);
     Ok(wm_try!(image.pixels.write_with_encoder(encoder)))
 }
@@ -22,18 +23,17 @@ pub fn encode<W: Write>(
 // https://www.imagemagick.org/script/command-line-options.php#quality
 fn quality_to_compression_parameters(
     quality: Option<f64>,
-) -> Result<(CompressionType, FilterType), MagickError> {
+) -> Result<(DeflateCompressionType, FilterType), MagickError> {
     if let Some(quality) = quality {
         if quality.is_sign_negative() {
             return Err(wm_err!("PNG quality cannot be negative"));
         }
         let quality = quality as u64;
-        // TODO: correct quality mapping is blocked on upstream issue:
-        // https://github.com/image-rs/image/issues/2495
+
         let compression = match quality / 10 {
-            0..=2 => CompressionType::Fast,
-            3..=7 => CompressionType::Default,
-            8.. => CompressionType::Best, // in imagemagick large values are treated as 9
+            0 => DeflateCompressionType::NoCompression,
+            n @ 1..=9 => DeflateCompressionType::Level(n as u8),
+            10.. => DeflateCompressionType::Level(9), // in imagemagick large values are treated as 9
         };
         let filter = match quality % 10 {
             0 => FilterType::NoFilter,
@@ -44,21 +44,22 @@ fn quality_to_compression_parameters(
             // 7 is documented as MNG-only, in practice maps to 5 or 6?
             5..=7 => FilterType::Adaptive,
             // filters 8 and 9 override compression level selection
-            8 => return Ok((CompressionType::Fast, FilterType::Adaptive)),
+            8 => return Ok((DeflateCompressionType::FdeflateUltraFast, FilterType::Adaptive)),
             // imagemagick uses filter=None here, but our Fast mode needs filtering
             // to deliver reasonable compression, so use the fastest filter instead
-            9 => return Ok((CompressionType::Fast, FilterType::Up)),
+            9 => return Ok((DeflateCompressionType::FdeflateUltraFast, FilterType::Up)),
             10.. => unreachable!(),
         };
 
-        if filter == FilterType::NoFilter && compression == CompressionType::Fast {
-            // CompressionType::Fast needs filtering for a reasonable compression ratio.
+        if filter == FilterType::NoFilter && compression == DeflateCompressionType::FdeflateUltraFast {
+            // DeflateCompressionType::Fast needs filtering for a reasonable compression ratio.
             // When using it, use the fastest filter instead of no filter at all.
-            Ok((CompressionType::Fast, FilterType::Up))
+            Ok((DeflateCompressionType::FdeflateUltraFast, FilterType::Up))
         } else {
             Ok((compression, filter))
         }
     } else {
-        Ok((CompressionType::Default, FilterType::Adaptive))
+        // default is 75 as per https://legacy.imagemagick.org/script/command-line-options.php#quality
+        Ok((DeflateCompressionType::Level(7), FilterType::Adaptive))
     }
 }
