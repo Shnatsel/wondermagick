20c5bb49f9afa17f7bd07d8aa9b284b9bf8c254f
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2025-09-30 21:46:20 +0200

    Make testable and add test

--- src/operations/identify.rs
+++ src/operations/identify.rs
@@ -1,6 +1,32 @@
 use crate::{error::MagickError, image::Image};
 
-pub fn identify(image: &mut Image) -> Result<(), MagickError> {
-    println!("{}x{}", image.pixels.width(), image.pixels.height(),);
-    Ok(())
+pub fn identify(image: &mut Image) -> Result<String, MagickError> {
+    Ok(format!(
+        "{}x{}",
+        image.pixels.width(),
+        image.pixels.height()
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use image::DynamicImage;
+
+    use quickcheck_macros::quickcheck;
+
+    #[quickcheck]
+    // u8::MAX * u8::MAX is a large enough space for
+    // quickcheck to explore and verify and still runs quickly
+    fn test_identify(width: u8, height: u8) -> bool {
+        let image = DynamicImage::new_rgba8((width as u32) + 1, (height as u32) + 1);
+        let info = identify(&mut Image {
+            format: image::ImageFormat::Png,
+            exif: None,
+            icc: None,
+            pixels: image,
+        })
+        .unwrap();
+        info == format!("{}x{}", width, height)
+    }
 }
--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -27,7 +27,12 @@ impl Operation {
             Operation::AutoOrient => auto_orient::auto_orient(image),
             Operation::Crop(geom) => crop::crop(image, geom),
             Operation::CropOnLoad(geom) => crop::crop_on_load(image, geom),
-            Operation::Identify => identify::identify(image),
+            Operation::Identify => {
+                let info = identify::identify(image)
+                    .unwrap_or_else(|e| format!("Failed to identify image: {}", e));
+                println!("{}", info);
+                Ok(())
+            }
             Operation::Resize(geom) => resize::resize(image, geom),
             Operation::Sample(geom) => resize::sample(image, geom),
             Operation::Scale(geom) => resize::scale(image, geom),
