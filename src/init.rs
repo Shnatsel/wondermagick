f0465575c597cde122fb64b5ee82a2e3d136e294
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2025-09-30 20:40:59 +0200

    Make `jxl` support optional too

--- src/init.rs
+++ src/init.rs
@@ -2,5 +2,6 @@
 
 /// Performs any global state initialization that needs to be done before performing image operations
 pub fn init() {
+    #[cfg(feature = "jxl")]
     jxl_oxide::integration::register_image_decoding_hook();
 }
