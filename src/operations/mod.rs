6c2e1d5ea4757a1d55d6932e2caa80465ccb14ba
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2025-09-30 21:59:46 +0200

    Revert enum case ordering

--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -11,32 +11,32 @@ use crate::{
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Operation {
-    AutoOrient,
-    Crop(CropGeometry),
-    CropOnLoad(LoadCropGeometry),
-    Identify,
     Resize(ResizeGeometry),
-    Sample(ResizeGeometry),
-    Scale(ResizeGeometry),
     Thumbnail(ResizeGeometry),
+    Scale(ResizeGeometry),
+    Sample(ResizeGeometry),
+    CropOnLoad(LoadCropGeometry),
+    Crop(CropGeometry),
+    Identify,
+    AutoOrient,
 }
 
 impl Operation {
     pub fn execute(&self, image: &mut Image) -> Result<(), MagickError> {
         match self {
-            Operation::AutoOrient => auto_orient::auto_orient(image),
-            Operation::Crop(geom) => crop::crop(image, geom),
+            Operation::Resize(geom) => resize::resize(image, geom),
+            Operation::Thumbnail(geom) => resize::thumbnail(image, geom),
+            Operation::Scale(geom) => resize::scale(image, geom),
+            Operation::Sample(geom) => resize::sample(image, geom),
             Operation::CropOnLoad(geom) => crop::crop_on_load(image, geom),
+            Operation::Crop(geom) => crop::crop(image, geom),
             Operation::Identify => {
                 let info = identify::identify(image)
                     .unwrap_or_else(|e| format!("Failed to identify image: {}", e));
                 println!("{}", info);
                 Ok(())
             }
-            Operation::Resize(geom) => resize::resize(image, geom),
-            Operation::Sample(geom) => resize::sample(image, geom),
-            Operation::Scale(geom) => resize::scale(image, geom),
-            Operation::Thumbnail(geom) => resize::thumbnail(image, geom),
+            Operation::AutoOrient => auto_orient::auto_orient(image),
         }
     }
 }
