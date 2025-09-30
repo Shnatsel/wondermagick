b02a7b449e56aac8c183551dafee40f9e9434b2b
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2025-09-30 21:54:42 +0200

    Make sure to only generate valid sizes

--- src/operations/identify.rs
+++ src/operations/identify.rs
@@ -14,12 +14,13 @@ mod tests {
     use image::DynamicImage;
 
     use quickcheck_macros::quickcheck;
+    use std::num::NonZeroU8;
 
     #[quickcheck]
     // u8::MAX * u8::MAX is a large enough space for
     // quickcheck to explore and verify and still runs quickly
-    fn test_identify(width: u8, height: u8) -> bool {
-        let image = DynamicImage::new_rgba8((width as u32) + 1, (height as u32) + 1);
+    fn test_identify(width: NonZeroU8, height: NonZeroU8) {
+        let image = DynamicImage::new_rgba8(width.get() as u32, height.get() as u32);
         let info = identify(&mut Image {
             format: image::ImageFormat::Png,
             exif: None,
@@ -27,6 +28,9 @@ mod tests {
             pixels: image,
         })
         .unwrap();
-        info == format!("{}x{}", width, height)
+        assert_eq!(
+            info,
+            format!("{}x{}", width.get() as u32, height.get() as u32)
+        );
     }
 }
