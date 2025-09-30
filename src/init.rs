f5ac3d02ae398badfb054481431792116396b680
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-09-30 16:17:06 +0100

    Update to the function name that actually shipped

--- src/init.rs
+++ src/init.rs
@@ -2,5 +2,5 @@
 
 /// Performs any global state initialization that needs to be done before performing image operations
 pub fn init() {
-    jxl_oxide::integration::register_decoding_hook();
+    jxl_oxide::integration::register_image_decoding_hook();
 }
