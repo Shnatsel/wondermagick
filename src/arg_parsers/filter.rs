4d6dcda6f510f3c20866ad8ea41ce147ba049a9c
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-11-14 19:44:53 +0000

    impl Display for Filter

--- src/arg_parsers/filter.rs
+++ src/arg_parsers/filter.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use pic_scale_safe::ResamplingFunction;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, strum::EnumString, strum::IntoStaticStr)]
@@ -35,6 +37,13 @@ pub enum Filter {
     Spline,
     Triangle,
     Welch,
+}
+
+impl Display for Filter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let stringified: &'static str = self.into();
+        f.write_str(stringified)
+    }
 }
 
 impl Filter {
