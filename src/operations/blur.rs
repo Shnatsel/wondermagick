0572c62c36ec396bba203dfd192121d38d170f90
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-11-30 12:29:02 +0000

    Add an internal function for gaussian blur

--- src/operations/blur.rs
+++ src/operations/blur.rs
@@ -4,3 +4,8 @@ pub fn blur(image: &mut Image, geometry: &BlurGeometry) -> Result<(), MagickErro
     image.pixels = image.pixels.fast_blur(geometry.sigma);
     Ok(())
 }
+
+pub fn gaussian_blur(image: &mut Image, geometry: &BlurGeometry) -> Result<(), MagickError> {
+    image.pixels = image.pixels.blur(geometry.sigma);
+    Ok(())
+}
