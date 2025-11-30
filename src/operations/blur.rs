04c8456220319f863be89269bb2c97e36e7886a5
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-11-30 12:25:43 +0000

    switch -blur over to fast_blur as per https://usage.imagemagick.org/blur/#blur_gaussian

--- src/operations/blur.rs
+++ src/operations/blur.rs
@@ -1,6 +1,6 @@
 use crate::{arg_parsers::BlurGeometry, error::MagickError, image::Image};
 
 pub fn blur(image: &mut Image, geometry: &BlurGeometry) -> Result<(), MagickError> {
-    image.pixels = image.pixels.blur(geometry.sigma);
+    image.pixels = image.pixels.fast_blur(geometry.sigma);
     Ok(())
 }
