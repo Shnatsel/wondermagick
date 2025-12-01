d9ef2869a57a70ccbefa7abc1f6f0e8798179f7e
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2025-12-01 21:57:52 +0100

    Allow changing the `-gravity`

--- src/operations/composite.rs
+++ src/operations/composite.rs
@@ -1,19 +1,52 @@
 use crate::{
-    arg_parsers::{FileFormat, Location},
+    arg_parsers::{FileFormat, Gravity, Location},
     decode::decode,
     error::MagickError,
     image::Image,
 };
-use image::imageops::overlay;
+use image::{imageops::overlay, GenericImageView};
 
 pub fn composite(
     image1: &mut Image,
     image2_location: &Location,
     image2_format: Option<FileFormat>,
-    //TODO: If a third image is given this is treated as a grayscale blending 'mask' image
+    gravity: Option<&Gravity>,
+    // TODO: If a third image is given this is treated as a grayscale blending 'mask' image
     // relative to the first 'destination' image. This mask is blended with the source image.
 ) -> Result<(), MagickError> {
     let image2 = decode(image2_location, image2_format)?;
-    overlay(&mut image1.pixels, &image2.pixels, 0, 0);
+    let (x, y) = if let Some(g) = gravity {
+        offset_from_gravity(g, image1.pixels.dimensions(), image2.pixels.dimensions())
+    } else {
+        (0, 0)
+    };
+    overlay(&mut image1.pixels, &image2.pixels, x as i64, y as i64);
     Ok(())
+}
+
+fn offset_from_gravity(
+    gravity: &Gravity,
+    image1_dim: (u32, u32),
+    image2_dim: (u32, u32),
+) -> (u32, u32) {
+    match gravity {
+        Gravity::Center => (
+            (image1_dim.0 - image2_dim.0) / 2,
+            (image1_dim.1 - image2_dim.1) / 2,
+        ),
+        Gravity::North => ((image1_dim.0 - image2_dim.0) / 2, 0),
+        Gravity::South => (
+            (image1_dim.0 - image2_dim.0) / 2,
+            image1_dim.1 - image2_dim.1,
+        ),
+        Gravity::East => (
+            image1_dim.0 - image2_dim.0,
+            (image1_dim.1 - image2_dim.1) / 2,
+        ),
+        Gravity::West => (0, (image1_dim.1 - image2_dim.1) / 2),
+        Gravity::NorthEast => (image1_dim.0 - image2_dim.0, 0),
+        Gravity::NorthWest => (0, 0),
+        Gravity::SouthEast => (image1_dim.0 - image2_dim.0, image1_dim.1 - image2_dim.1),
+        Gravity::SouthWest => (0, image1_dim.1 - image2_dim.1),
+    }
 }
