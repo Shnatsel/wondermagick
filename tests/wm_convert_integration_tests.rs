cf967f3f212faa7ba7eca2c447bac6bd386d6ec6
A. Molzer <5550310+197g@users.noreply.github.com>
2025-12-09 22:37:39 +0100

    Add integration test for -write operation

--- tests/wm_convert_integration_tests.rs
+++ tests/wm_convert_integration_tests.rs
@@ -40,3 +40,36 @@ fn test_resize_identify_succeeds() {
     assert!(convert.status.success());
     assert!(String::from_utf8(identify.stdout).unwrap().contains("5x5"));
 }
+
+#[test]
+fn test_write_as_intermediate() {
+    let binary = env!("CARGO_BIN_EXE_wm-convert");
+    let tmp_dir = env!("CARGO_TARGET_TMPDIR");
+
+    let output_path = format!("{}/resized.jpg", tmp_dir);
+    let copy_path = format!("{}/copy.png", tmp_dir);
+    let _ = fs::remove_file(&output_path);
+    let _ = fs::remove_file(&copy_path);
+
+    let result = Command::new(binary)
+        .args(&[
+            "./tests/sample.png",
+            "-write",
+            &copy_path,
+            "-resize",
+            "50%x50%",
+            &output_path,
+        ])
+        .output()
+        .expect("convert did not exit successfully");
+
+    assert!(result.status.success());
+    assert!(std::path::Path::new(&output_path).exists());
+    assert!(std::path::Path::new(&copy_path).exists());
+
+    let copy_img = image::open(&copy_path).unwrap();
+    let output_img = image::open(&output_path).unwrap();
+
+    use image::GenericImageView as _;
+    assert_ne!(copy_img.dimensions(), output_img.dimensions());
+}
