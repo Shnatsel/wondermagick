9c7c4491637723b1bb93bd59187b1f4f90cdc2a1
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-01-28 21:32:38 +0000

    Write Exif metadata to AVIF files

--- src/encoders/avif.rs
+++ src/encoders/avif.rs
@@ -1,8 +1,8 @@
 use std::io::Write;
 
 use image::codecs::avif::AvifEncoder;
-use image::ImageEncoder;
 
+use crate::encoders::common::write_icc_and_exif;
 use crate::{error::MagickError, image::Image, plan::Modifiers, wm_try};
 
 pub fn encode<W: Write>(
@@ -13,9 +13,7 @@ pub fn encode<W: Write>(
     // TODO: quality conversion might not be bug-compatible with imagemagick
     let quality = modifiers.quality.map(|q| q as u8).unwrap_or(50);
     let mut encoder = AvifEncoder::new_with_speed_quality(writer, 4, quality);
-    if let Some(icc) = image.icc.clone() {
-        let _ = encoder.set_icc_profile(icc); // ignore UnsupportedError
-    };
+    write_icc_and_exif(&mut encoder, image);
     // ravif already discards alpha channel automatically if all pixels are opaque,
     // so no need to explicitly convert on our end
     wm_try!(image.pixels.write_with_encoder(encoder));
